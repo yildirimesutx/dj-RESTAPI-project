11f199c817276c25121711e7b5de53873cc84e25
mesut <yildirimesutcw@gmail.com>
2022-08-02 23:10:30 +0300

    func views crud

--- haberler/api/urls.py
+++ haberler/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import makale_list_create_api_view
+from .views import makale_list_create_api_view, makale_detail_api_view
+
+
 
 
 urlpatterns = [
-    path("makaleler/", makale_list_create_api_view, name="makale-listesi" )
+    path("makaleler/", makale_list_create_api_view, name="makale-listesi" ),
+    path('makaleler/<int:pk>/', makale_detail_api_view, name="makale-detay")
 ]
+ 
